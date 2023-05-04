f5c923ca7b370432985340071da813d70560e37c
Pablo Rodriguez <pabrodsan11@alum.us.es>
2023-05-04 20:44:30 +0200

    Initial commit

---
+++ src/test/java/Miner/GitHub/GitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package Miner.GitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
